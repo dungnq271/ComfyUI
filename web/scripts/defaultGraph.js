1b840bc63fd229c253c73a85a59b5d4207d1788b
dungnq271 <dungnguyen2712002@gmail.com>
2024-07-07 22:53:38 +0700

    Modify the default graph

--- web/scripts/defaultGraph.js
+++ web/scripts/defaultGraph.js
@@ -14,7 +14,7 @@ export const defaultGraph = {
       order: 2,
       mode: 0,
       outputs: [{
-        name: "query",
+        name: "user_message",
         type: "TEXT",
         links: [1],
         slot_index: 0
@@ -24,43 +24,43 @@ export const defaultGraph = {
 	"Who is Elon Musk?"
       ],
     },
+    // {
+    //   id: 2,
+    //   type: "LLM",
+    //   pos: [1235, 186],
+    //   size: {
+    //     0: 422.84503173828125,
+    //     1: 164.31304931640625
+    //   },
+    //   flags: {},
+    //   order: 2,
+    //   mode: 0,
+    //   inputs: [
+    // 	{
+    //       name: "prompt",
+    //       type: "TEXT",
+    //       link: 2
+    // 	}	
+    //   ],
+    //   outputs: [{
+    //     name: "text",
+    //     type: "TEXT",
+    //     links: [3],
+    //     slot_index: 0
+    //   }],
+    //   properties: {},
+    //   widgets_values: [
+    // 	"claude-3-haiku-20240307",
+    // 	"You are an agent designed to summarize a paragraph."
+    //   ],
+    // },
     {
       id: 2,
-      type: "LLM",
-      pos: [1235, 186],
-      size: {
-        0: 422.84503173828125,
-        1: 164.31304931640625
-      },
-      flags: {},
-      order: 2,
-      mode: 0,
-      inputs: [
-	{
-          name: "prompt",
-          type: "TEXT",
-          link: 2
-	}	
-      ],
-      outputs: [{
-        name: "text",
-        type: "TEXT",
-        links: [3],
-        slot_index: 0
-      }],
-      properties: {},
-      widgets_values: [
-	"gpt-3.5-turbo",
-	"You are an agent designed to summarize a paragraph."
-      ],
-    },
-    {
-      id: 3,
-      type: "WikipediaSearch",
+      type: "Search",
       pos: [815, 189],
       size: {
-        0: 210,
-        1: 26
+        0: 315,
+        1: 50
       },
       flags: {},
       order: 6,
@@ -71,40 +71,43 @@ export const defaultGraph = {
         link: 1
       }],
       outputs: [{
-        name: "prompt",
-        type: "TEXT",
+        name: "nodes",
+        type: "NODE",
         links: [2],
         slot_index: 0
       }],      
       properties: {},
       widgets_values: [
-	"en",
-	3
+	"wikipedia",
       ],      
     },    
     {
-      id: 4,
-      type: "End",
-      pos: [1754, 189],
+      id: 3,
+      type: "Response",
+      // pos: [1754, 189],
+      pos: [1435, 186],
       size: {
-        0: 210,
-        1: 26
+        0: 422,
+        1: 164
       },
       flags: {},
       order: 6,
       mode: 0,
       inputs: [{
-        name: "text",
-        type: "TEXT",
-        link: 3
+        name: "nodes",
+        type: "NODE",
+        link: 2
       }],
       properties: {},
+      widgets_values: [
+	"claude-3-haiku-20240307",
+	// "You are an agent designed to summarize a paragraph."
+      ],      
     },    
   ],
   links: [
-    [1, 1, 0, 3, 0, "SEARCH"],
-    [2, 3, 0, 2, 0, "LLM"],
-    [3, 2, 0, 4, 0, "TEXT"],    
+    [1, 1, 0, 2, 0, "SEARCH"],
+    [2, 2, 0, 3, 0, "RESPONSE"],
   ],
   groups: [],
   config: {},
